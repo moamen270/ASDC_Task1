307e1d47d81c80e54cd4ce98583b4c33fefba662
Moamen Mostafa <mmoamen270@gmail.com>
2023-11-21 13:56:48 +0200

    Change DTO

---
+++ Task1/Entity/DTO/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Task1.Entity.DTO
+{
+    public record ProductDto
+    {
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string LocationFind { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Color { get; set; }
+    }
+}
